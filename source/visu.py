87ac7c8a512e87bbf9207d8e4290e8a8c232db02
Marcel <marcel.skrok@onet.pl>
2025-01-19 17:26:31 +0100

    Added window rescaling

--- source/visu.py
+++ source/visu.py
@@ -4,32 +4,9 @@ import port_data_pb2 as port
 import zmq
 
 dpg.create_context()
-
-cart_xpos = [125, 125, 125, 125, 1000]
-cart_ypos = [300, 470, 640, 810, 690]
-
-# polozenia indykatorow
-ind_xpos = [125, 125, 125, 125, 1000, 1800]
-ind_ypos = [300, 470, 640, 810, 690, 520]
-
-cart_pos = {
-    port.Cart.CartPosition.AFRICA_LP: [425, 285],
-    port.Cart.CartPosition.AFRICA_WAITING: [540, 335],
-    port.Cart.CartPosition.EUROPA_LP: [425, 430],
-    port.Cart.CartPosition.EUROPA_WAITING: [540, 480],
-    port.Cart.CartPosition.ASIA_LP: [425, 585],
-    port.Cart.CartPosition.ASIA_WAITING: [540, 635],
-    port.Cart.CartPosition.AMERICA_LP: [425, 745],
-    port.Cart.CartPosition.AMERICA_WAITING: [540, 795],
-    port.Cart.CartPosition.ST_LP1: [880, 285],
-    port.Cart.CartPosition.ST_LP2: [1105, 285],
-    port.Cart.CartPosition.ST_WAITING: [625, 165],
-    port.Cart.CartPosition.SHIP_LP1: [1560, 320],
-    port.Cart.CartPosition.SHIP_LP2: [1560, 520],
-    port.Cart.CartPosition.SHIP_LP3: [1560, 715],
-    port.Cart.CartPosition.SHIP_WAITIMG: [1445, 270],
-}
-tst = cart_pos[port.Cart.CartPosition.ST_LP2]
+# default main img size - W:2048, H:1080
+WINDOW_WIDTH = 2000
+WINDOW_HEIGTH = int(WINDOW_WIDTH / 2)
 
 
 class Port:
@@ -125,9 +102,40 @@ class Port:
 
 myPort = Port()
 myPort.loadImages()
-mainWindowWidth = myPort.images[myPort.imagesNames[0]][0]
-mainWindowHeight = myPort.images[myPort.imagesNames[0]][1]
+mainWindowWidth = WINDOW_WIDTH
+mainWindowHeight = WINDOW_HEIGTH + 50 #offset for a toolbar
 
+X_SCALING_FACTOR = float(WINDOW_WIDTH)/float(myPort.images[myPort.imagesNames[0]][0])
+Y_SCALING_FACTOR = float(WINDOW_HEIGTH)/float(myPort.images[myPort.imagesNames[0]][1])
+IND_WIDTH = 80
+IND_HEIGTH = 25
+SHIP_BEGINING_X = 1800
+SHIP_BEGINING_Y = 100
+
+# polozenia indykatorow
+ind_xpos = [125, 125, 125, 125, 1000, 1900]
+ind_ypos = [350, 520, 690, 860, 690, 520]
+
+ind_xpos = [int((element + IND_WIDTH/2) * X_SCALING_FACTOR - IND_WIDTH/2) for element in ind_xpos]
+ind_ypos = [int((element + IND_HEIGTH/2) * Y_SCALING_FACTOR - IND_HEIGTH/2) for element in ind_ypos]
+
+cart_pos = {
+    port.Cart.CartPosition.AFRICA_LP: [int(425*X_SCALING_FACTOR), int(310*Y_SCALING_FACTOR)],
+    port.Cart.CartPosition.AFRICA_WAITING: [int(540*X_SCALING_FACTOR), int(360*Y_SCALING_FACTOR)],
+    port.Cart.CartPosition.EUROPA_LP: [int(425*X_SCALING_FACTOR), int(455*Y_SCALING_FACTOR)],
+    port.Cart.CartPosition.EUROPA_WAITING: [int(540*X_SCALING_FACTOR), int(505*Y_SCALING_FACTOR)],
+    port.Cart.CartPosition.ASIA_LP: [int(425*X_SCALING_FACTOR), int(610*Y_SCALING_FACTOR)],
+    port.Cart.CartPosition.ASIA_WAITING: [int(540*X_SCALING_FACTOR), int(660*Y_SCALING_FACTOR)],
+    port.Cart.CartPosition.AMERICA_LP: [int(425*X_SCALING_FACTOR), int(770*Y_SCALING_FACTOR)],
+    port.Cart.CartPosition.AMERICA_WAITING: [int(540*X_SCALING_FACTOR), int(820*Y_SCALING_FACTOR)],
+    port.Cart.CartPosition.ST_LP1: [int(875*X_SCALING_FACTOR), int(310*Y_SCALING_FACTOR)],
+    port.Cart.CartPosition.ST_LP2: [int(1100*X_SCALING_FACTOR), int(310*Y_SCALING_FACTOR)],
+    port.Cart.CartPosition.ST_WAITING: [int(625*X_SCALING_FACTOR), int(190*Y_SCALING_FACTOR)],
+    port.Cart.CartPosition.SHIP_LP1: [int(1560*X_SCALING_FACTOR), int(345*Y_SCALING_FACTOR)],
+    port.Cart.CartPosition.SHIP_LP2: [int(1560*X_SCALING_FACTOR), int(545*Y_SCALING_FACTOR)],
+    port.Cart.CartPosition.SHIP_LP3: [int(1560*X_SCALING_FACTOR), int(740*Y_SCALING_FACTOR)],
+    port.Cart.CartPosition.SHIP_WAITIMG: [int(1445*X_SCALING_FACTOR), int(295*Y_SCALING_FACTOR)],
+}
 
 def updateGUIState(sender, data):
     while True:
@@ -139,22 +147,24 @@ with dpg.window(
     tag="mainWindow",
     label="Port w Rotterdamie ale lepiej",
     width=mainWindowWidth,
-    height=mainWindowHeight - 10,
+    height=mainWindowHeight,
 ):
-
-    dpg.add_image(texture_tag=myPort.imagesNames[0], pos=[0, 0])
-    dpg.add_image(
-        texture_tag=myPort.imagesNames[1],
-        tag=f"{cfg.containers[5]}_image",
-        pos=[1800, 100],
-    )
+    with dpg.drawlist(width=WINDOW_WIDTH, height=WINDOW_HEIGTH):
+        dpg.draw_image(myPort.imagesNames[0], 
+                        (0, 0), (WINDOW_WIDTH, WINDOW_HEIGTH), 
+                        uv_min=(0, 0), uv_max=(1, 1))
+        dpg.draw_image(myPort.imagesNames[1], 
+                        (int(SHIP_BEGINING_X*X_SCALING_FACTOR), int(SHIP_BEGINING_Y*Y_SCALING_FACTOR)), 
+                        (WINDOW_WIDTH, WINDOW_HEIGTH), 
+                        uv_min=(0, 0), uv_max=(1, 1),
+                        tag=f"{cfg.containers[5]}_image")
 
     for i in range(len(cfg.containers)):
         dpg.add_progress_bar(
             tag=f"{cfg.containers[i]}_ind",
             pos=[ind_xpos[i], ind_ypos[i]],
-            width=80,
-            height=25,
+            width=IND_WIDTH,
+            height=IND_HEIGTH,
         )
 
     cartPositions = list(cart_pos.values())
@@ -163,7 +173,7 @@ with dpg.window(
             texture_tag=myPort.imagesNames[2], tag=f"cart_{i}", pos=cartPositions[i]
         )
 
-with dpg.window(tag="controlWindow"):
+with dpg.window(tag="controlWindow", pos = [0,0], min_size=[10, 10]):
     dpg.add_button(label="Update", callback=updateGUIState)
 
 dpg.create_viewport(title="InPort App", width=mainWindowWidth, height=mainWindowHeight)
